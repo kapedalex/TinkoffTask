b15c0a926b0199a2d1ba4ec6a02b1da7e0feb2de
kapedalex <72311866+kapedalex@users.noreply.github.com>
2022-09-05 20:39:07 +0300

    fix: upload resources

---
+++ Resources/Constants.py
@@ -0,0 +1,8 @@
+class Constants:
+    SAVE_PATH_MODEL = 'Resources/model/model'
+    SAVE_PATH_OPTIMIZER = 'Resources/model/optimiser'
+    SAVE_PATH_LOSS = 'Resources/model/train_losses'
+    TRAIN_TEXT_FILE_PATH = 'Resources/Master-i-margarita.txt'
+    GREETING = 'Type 1 to train the model, 2 to evaluate existing one.'
+    ERROR = 'ERROR'
+    TRAINING = 'Training...'
